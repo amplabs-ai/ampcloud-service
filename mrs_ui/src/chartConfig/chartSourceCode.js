af05d03a10dba32de827c2f8e07d40119ca12757
Hema-Appperfect <hema@appperfect.com>
2022-03-29 18:04:15 +0530

    Added mappings to add data to charts.

--- mrs_ui/src/chartConfig/chartSourceCode.js
+++ mrs_ui/src/chartConfig/chartSourceCode.js
@@ -1,5 +1,5 @@
 const sourceCode = {
-	cycleIndexChart: `
+  cycleIndexChart: `
     '''
     Install required dependencies
     >>>pip3 install pandas
@@ -16,8 +16,8 @@ const sourceCode = {
     httprequest = urllib.request.Request(
             url, method="GET"
         )
-    httprequest.add_header("Cookie", "{1}")
-    status = 1
+    httprequest.add_header("Cookie", "userId={1}")
+    status = 0
     try:
         with urllib.request.urlopen(httprequest) as httpresponse:
             response = json.loads(httpresponse.read())
@@ -29,7 +29,7 @@ const sourceCode = {
         for item in response['records'][0]:
             print(item)
             df = pd.DataFrame.from_records(item['source'])
-            plt.plot(df['cycle_index'], df['value'], label = item['id'])
+            plt.plot(df['cycle_index'], pd.to_numeric(df['value'], errors='coerce'), label = item['id'])
 
         plt.title('Cycle Index Data - Enery and Capacity Decay', loc="left")
         plt.xlabel('Cycle Index')
@@ -37,61 +37,153 @@ const sourceCode = {
         plt.legend()
         plt.show()
   `,
-	timeSeriesChart: `
-  time
-    from matplotlib import pyplot as plt
-    import numpy as np
+  timeSeriesChart: `
+    '''
+    Install required dependencies
+    >>>pip3 install pandas
+    >>>pip3 install matplotlib
+    '''
 
-    # Generate 100 random data points along 3 dimensions
-    x, y, scale = np.random.randn(3, 100)
-    fig, ax = plt.subplots()
+    import json
+    import urllib.error
+    import urllib.request
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
-    # Map each onto a scatterplot we'll create with Matplotlib
-    ax.scatter(x=x, y=y, c=scale, s=np.abs(scale)*500)
-    ax.set(title="Some random data, created with JupyterLab!")
-    plt.show()XZx
+    url = "http://batteryarchivemrstutoriallb-436798068.ap-south-1.elb.amazonaws.com:81/echarts/energyAndCapacityDecay?{0}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", "userId={1}")
+    status = 0
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            status = 1
+    except urllib.error.HTTPError as e:
+        print(e)
+
+    if status:
+        for item in response['records'][0]:
+            df = pd.DataFrame.from_records(item['source'])
+            plt.scatter(df['test_time'], pd.to_numeric(df['value'], errors='coerce'), label = item['id'])
+
+        plt.title('Time Series Data - Energy and Capacity Decay', loc="left")
+        plt.xlabel('Time (s)')
+        plt.ylabel('Wh/Ah')
+        plt.legend()
+        plt.show()
   `,
-	efficiencyChart: `
-  eff
-    from matplotlib import pyplot as plt
-    import numpy as np
+  efficiencyChart: `
+    '''
+    Install required dependencies
+    >>>pip3 install pandas
+    >>>pip3install matplotlib
+    '''
 
-    # Generate 100 random data points along 3 dimensions
-    x, y, scale = np.random.randn(3, 100)
-    fig, ax = plt.subplots()
+    import json
+    import urllib.error
+    import urllib.request
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
-    # Map each onto a scatterplot we'll create with Matplotlib
-    ax.scatter(x=x, y=y, c=scale, s=np.abs(scale)*500)
-    ax.set(title="Some random data, created with JupyterLab!")
-    plt.show()XZx
+    url = "http://batteryarchivemrstutoriallb-436798068.ap-south-1.elb.amazonaws.com:81/echarts/efficiency?{0}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", "userId={1}")
+    status = 0
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            status = 1
+    except urllib.error.HTTPError as e:
+        print(e)
+
+    if status:
+        for item in response['records'][0]:
+            df = pd.DataFrame.from_records(item['source'])
+            plt.plot(df['cycle_index'], pd.to_numeric(df['value'], errors='coerce'), label = item['id'])
+
+        plt.title('Efficiencies', loc="left")
+        plt.xlabel('Cycle Index')
+        plt.ylabel('Enery and Coulombic Efficiencies')
+        plt.legend()
+        plt.show()
   `,
-	cycleQtyByStepChart: `
-  bystep
-    from matplotlib import pyplot as plt
-    import numpy as np
+  cycleQtyByStepChart: `
+    '''
+    Install required dependencies
+    >>>pip3 install pandas
+    >>>pip3 install matplotlib
+    '''
 
-    # Generate 100 random data points along 3 dimensions
-    x, y, scale = np.random.randn(3, 100)
-    fig, ax = plt.subplots()
+    import json
+    import urllib.error
+    import urllib.request
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
-    # Map each onto a scatterplot we'll create with Matplotlib
-    ax.scatter(x=x, y=y, c=scale, s=np.abs(scale)*500)
-    ax.set(title="Some random data, created with JupyterLab!")
-    plt.show()XZx
+    url = "http://batteryarchivemrstutoriallb-436798068.ap-south-1.elb.amazonaws.com:81/echarts/cycleQuantitiesByStep?{0}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", "userId={1}")
+    status = 0
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            status = 1
+    except urllib.error.HTTPError as e:
+        print(e)
+
+    if status:
+        for item in response['records'][0]:
+            df = pd.DataFrame.from_records(item['source'])
+            plt.plot(df['cycle_time'], df['v'], label = item['id'])
+
+        plt.title('Cycle Quantities by step', loc="left")
+        plt.xlabel('Cycle Tiime (s)')
+        plt.ylabel('Volateg (V)')
+        plt.legend()
+        plt.show()
     `,
-	compareByCycleTimeChart: `
-  cycletime
-    from matplotlib import pyplot as plt
-    import numpy as np
+  compareByCycleTimeChart: `
+    '''
+    Install required dependencies
+    >>>pip3 install pandas
+    >>>pip3install matplotlib
+    '''
 
-    # Generate 100 random data points along 3 dimensions
-    x, y, scale = np.random.randn(3, 100)
-    fig, ax = plt.subplots()
+    import json
+    import urllib.error
+    import urllib.request
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
-    # Map each onto a scatterplot we'll create with Matplotlib
-    ax.scatter(x=x, y=y, c=scale, s=np.abs(scale)*500)
-    ax.set(title="Some random data, created with JupyterLab!")
-    plt.show()XZx
+    url = "http://batteryarchivemrstutoriallb-436798068.ap-south-1.elb.amazonaws.com:81/echarts/compareByCycleTime?{0}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", "userId={1}")
+    status = 0
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            status = 1
+    except urllib.error.HTTPError as e:
+        print(e)
+
+    if status:
+        for item in response['records'][0]:
+            df = pd.DataFrame.from_records(item['source'])
+            plt.plot(df['cycle_time'], pd.to_numeric(df['value'], errors='coerce'), label = item['id'])
+
+        plt.title('Compare by Cycle Time - Compare Cycle Voltage and Current', loc="left")
+        plt.xlabel('Cycle time (s)')
+        plt.ylabel('Voltage (V)/Current (A)')
+        plt.legend()
+        plt.show()
   `,
 };
 
