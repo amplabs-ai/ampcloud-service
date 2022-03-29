52e2dbd2c4963325710b3e28405b727794a949d1
Hema-Appperfect <hema@appperfect.com>
2022-03-29 18:04:15 +0530

    Added mappings to add data to charts.

--- mrs_ui/src/components/ViewCodeModal.js
+++ mrs_ui/src/components/ViewCodeModal.js
@@ -10,65 +10,67 @@ import Cookies from "js-cookie";
 const { Text } = Typography;
 
 const ViewCodeModal = ({
-	code,
-	modalVisible,
-	setModalVisible,
-	searchParams,
+  code,
+  modalVisible,
+  setModalVisible,
+  searchParams,
 }) => {
-	const codeString = code;
+  const codeString = code;
 
-	const formatCode = (code, ...args) => {
-		for (let k in args) {
-			code = code.replace("{" + k + "}", args[k]);
-		}
-		return code;
-	};
+  const formatCode = (code, ...args) => {
+    for (let k in args) {
+      code = code.replace("{" + k + "}", args[k]);
+    }
+    return code;
+  };
 
-	return (
-		<>
-			<Modal
-				centered
-				visible={modalVisible}
-				onCancel={() => setModalVisible(false)}
-				footer={null}
-				width="fit-content"
-				bodyStyle={{
-					padding: "0px",
-				}}
-			>
-				<SyntaxHighlighter
-					onClick={() => {
-						console.log("copied!");
-						message.success("Copied to clipboard!");
-						message.success("Copied to clipboard!");
-						navigator.clipboard.writeText(codeString);
-					}}
-					showLineNumbers={true}
-					wrapLines={true}
-					language="python"
-					style={a11yLight}
-				>
-					{/* {codeString.replace("{queryParams}", searchParams)} */}
-					{formatCode(codeString, searchParams, Cookies.get("userId"))}
-				</SyntaxHighlighter>
-				<div className="d-flex flex-row-reverse mb-2">
-					<Text
-						type="secondary"
-						onClick={() => {
-							navigator.clipboard.writeText(codeString);
-							message.success("Copied to clipboard!");
-							message.success("Copied to clipboard!");
-							console.log("copied!");
-						}}
-						code={true}
-						style={{ cursor: "pointer" }}
-					>
-						Click to copy
-					</Text>
-				</div>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <Modal
+        centered
+        visible={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        footer={null}
+        width="fit-content"
+        bodyStyle={{
+          padding: "0px",
+        }}
+      >
+        <SyntaxHighlighter
+          onClick={() => {
+            console.log("copied!");
+            message.success("Copied to clipboard!");
+            message.success("Copied to clipboard!");
+            navigator.clipboard.writeText(codeString);
+          }}
+          showLineNumbers={true}
+          wrapLines={true}
+          language="python"
+          style={a11yLight}
+        >
+          {/* {codeString.replace("{queryParams}", searchParams)} */}
+          {formatCode(codeString, searchParams, Cookies.get("userId"))}
+        </SyntaxHighlighter>
+        <div className="d-flex flex-row-reverse mb-2">
+          <Text
+            type="secondary"
+            onClick={() => {
+              navigator.clipboard.writeText(
+                formatCode(codeString, searchParams, Cookies.get("userId"))
+              );
+              message.success("Copied to clipboard!");
+              message.success("Copied to clipboard!");
+              console.log("copied!");
+            }}
+            code={true}
+            style={{ cursor: "pointer" }}
+          >
+            Click to copy
+          </Text>
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 export default ViewCodeModal;
