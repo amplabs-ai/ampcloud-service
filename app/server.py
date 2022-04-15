aab1e43a734869faaccaa9ba76244b27cd6e89f0
neeraj-appperfect <neeraj_rathor@appperfect.com>
2022-04-15 12:21:14 +0000

    Serve static react code from flask server.

--- app/server.py
+++ app/server.py
@@ -1,14 +1,14 @@
 import connexion
 from connexion import ProblemException
 import os 
-from flask import jsonify 
+from flask import jsonify, render_template
 from sqlalchemy import create_engine
 from app.exception_handler import *
 from app.model import Model
 from flask_cors import CORS
 from flask_compress import Compress
 import logging
-# from celery import Celery
+# from celery import Celery 
 
 # Create and configure logger
 logging.basicConfig(filename="logs/audit.log",
@@ -24,11 +24,19 @@ app.app.config['DATABASE_CONNECT_OPTIONS'] = {}
 app.add_error_handler(404, client_exception)
 app.add_error_handler(400, client_exception)
 app.add_error_handler(ProblemException, problem_exception)
-CORS(app.app, origins=["http://www.amplabs.ai"], supports_credentials=True)
+# CORS(app.app, origins=["http://3.108.215.32:3000"], supports_credentials=True)
 Compress(app.app)
 print("Connected to database: {}".format(app.app.config['DATABASE_URI']))
 
 if __name__ == "__main__":
     engine_ = create_engine(app.app.config['DATABASE_URI'], echo=True)
     Model.metadata.create_all(engine_)
+    @app.route("/")
+    def my_index():
+        return render_template("index.html", flask_token="amplabs token")
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def index(path):
+        return render_template('index.html')
     app.run(debug=True, host='0.0.0.0', port='4000')
