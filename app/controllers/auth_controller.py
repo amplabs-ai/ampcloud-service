03c546965e3ed09b93764220d37e029cd5d2c84f
neeraj-appperfect <neeraj_rathor@appperfect.com>
2022-04-18 15:30:12 +0000

    Added logs in flask backend

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -11,14 +11,16 @@ def login():
     status, detail = login_service(email)
     resp =  make_response(Response(status, detail).to_dict())
     resp.set_cookie('userId',email)
-    logging.info("User {} login".format(email))
+    logging.info("User {} Action LOGIN".format(email))
     return resp, status
 
 def logout():
     email = request.cookies.get('userId')
     resp = make_response(Response(200,"Logout").to_dict())
     resp.delete_cookie('userId')
-    logging.info("User {} logout".format(email))
+    if (not email):
+        email = "someone@host.com"
+    logging.info("User {} Action LOGOUT".format(email))
     return resp
 
 def health_check():
