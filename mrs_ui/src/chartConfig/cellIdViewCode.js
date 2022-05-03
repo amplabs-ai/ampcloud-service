b980bc770677a31a2c606e2e18d2b089cb0e6866
Hema-Appperfect <hema@appperfect.com>
2022-05-03 14:42:42 +0530

    Frontend: Refactored code for view code section.

--- mrs_ui/src/chartConfig/cellIdViewCode.js
+++ mrs_ui/src/chartConfig/cellIdViewCode.js
@@ -1,56 +1,80 @@
 export const cycleDataCodeContent = `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 
-url = "http://www.amplabs.ai/download/cells/cycle_data_json/{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 0
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
+def get_amplabs_data():
+    url = "https://www.amplabs.ai/download/cells/cycle_data_json/{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
 
-if status and response['records']:
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+#Fetch Data from Amplabs API
+response = get_amplabs_data()
+
+if response:
     df = pd.DataFrame(response['records'])
     print("Process complete", df.info())
 `;
 export const timeSeriesDataCodeContent = `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 
-url = "http://www.amplabs.ai/download/cells/cycle_timeseries_json/{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 0
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
+def get_amplabs_data():
+    url = "http://www.amplabs.ai/download/cells/cycle_timeseries_json/{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
 
-if status and response['records']:
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+#Fetch Data from Amplabs API
+response = get_amplabs_data()
+
+if response:
     df = pd.DataFrame(response['records'])
     print("Process complete", df.info())
 `;
--- mrs_ui/src/chartConfig/chartSourceCode.js
+++ mrs_ui/src/chartConfig/chartSourceCode.js
@@ -1,291 +1,450 @@
 const sourceCode = {
-	cycleIndexChart: `
+  cycleIndexChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/energyAndCapacityDecay?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 0
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/energyAndCapacityDecay?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
 
-df = pd.DataFrame()
-if status:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.line(df, x="cycle_index", y="value", color="series", labels={"cycle_index":"Cycle Index", "value":"Ah/Wh"}, title = "Cycle Index Data - Energy and Capacity Decay")
     fig.update_traces(mode="markers+lines", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./cycleIndexChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	timeSeriesChart: `
+  timeSeriesChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/energyAndCapacityDecay?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 1
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
-if status:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/energyAndCapacityDecay?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.scatter(df, x="test_time", y="value", color="series", labels={"test_time":"Time (s)", "value":"Wh/Ah"}, title = "Time Series Data - Energy and Capacity Decay")
     fig.update_traces(mode="markers", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./timeSeriesChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	efficiencyChart: `
+  efficiencyChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/efficiency?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 1
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
-if status:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/efficiency?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.line(df, x="cycle_index", y="value", color="series", labels={"cycle_index":"Cycle Index", "value":"Enery and Coulombic Efficiencies"}, title = "Efficiencies")
     fig.update_traces(mode="markers+lines", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./efficiencyChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	cycleQtyByStepChart: `
+  cycleQtyByStepChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/cycleQuantitiesByStep?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 1
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
-if status:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/cycleQuantitiesByStep?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.line(df, x="cycle_time", y="v", color="series", labels={"cycle_time":"Cycle Tiime (s)", "v":"Volateg (V)"}, title = "Cycle Quantities by step")
     fig.update_traces(mode="markers+lines", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./cycleQtyByStepChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	compareByCycleTimeChart: `
+  compareByCycleTimeChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/compareByCycleTime?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 1
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
-if status:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/compareByCycleTime?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.line(df, x="cycle_time", y="value", color="series_2", labels={"cycle_time":"Cycle Time (s)", "value":"Voltage (V)/Current (A)"}, title = "Compare by Cycle Time - Compare Cycle Voltage and Current")
     fig.update_traces(mode="markers+lines", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./compareByCycleTimeChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	forceAndDisplacementChart: `
+  forceAndDisplacementChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/forceAndDisplacement?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 0
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/forceAndDisplacement?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
 
-if status and response['records'][0]:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.scatter(df, x="test_time", y="value", color="series", labels={"test_time":"Time (s)", "value":"Force (N) / Displacement"}, title = "Force and Displacement - Abuse Force and Displacement")
     fig.update_traces(mode="markers", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./abuseForceDisplacementChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	testTempraturesChart: `
+  testTempraturesChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/testTempratures?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 0
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
-if status and response['records'][0]:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/testTempratures?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.line(df, x="test_time", y="value", color="series_1", labels={"test_time":"Time (s)", "value":"Temperature (T)"}, title = "Abuse Test Temperature")
     fig.update_traces(mode="lines", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./abuseTestTemperatureChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
-	voltageChart: `
+  voltageChart: `
+# Download python packages to your system using pip install
 import sys
 !{sys.executable} -m pip install pandas plotly kaleido
+
+#suppressing warnings in Jupyter Notebooks
 import warnings
 warnings.filterwarnings('ignore')
 
+# Useful for fetching data from the web 
 import json
 import urllib.error
 import urllib.request
+
+# PyData Libraries
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-url = "http://www.amplabs.ai/echarts/voltage?{0}"
-httprequest = urllib.request.Request(
-        url, method="GET"
-    )
-httprequest.add_header("Cookie", "userId={1}")
-status = 0
-try:
-    with urllib.request.urlopen(httprequest) as httpresponse:
-        response = json.loads(httpresponse.read())
-        status = 1
-except urllib.error.HTTPError as e:
-    print(e)
 
-df = pd.DataFrame()
-if status and response['records'][0]:
-    for item in response['records'][0]:
-        df = df.append(pd.DataFrame.from_records(item['source']))
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+def get_amplabs_chartdata():
+    url = "https://www.amplabs.ai/echarts/voltage?{0}"
+    user = "{1}"
+    httprequest = urllib.request.Request(
+            url, method="GET"
+        )
+    httprequest.add_header("Cookie", f"userId={user}")
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = json.loads(httpresponse.read())
+            return response
+    except urllib.error.HTTPError as e:
+        print(e)
+    return None
+
+def plot_chart(df):
     fig = px.scatter(df, x="test_time", y="value", color="series", labels={"test_time":"Time (s)", "value":"Voltage (V)"}, title = "Voltage - Abuse Voltage")
     fig.update_traces(mode="markers", hovertemplate=None)
     fig.update_layout(hovermode="x")
     pio.write_image(fig, file='./abuseVoltageChart.png', format="png", scale=1, width=1200, height=800)
+
+
+#Fetch Data from Amplabs API
+response = get_amplabs_chartdata()
+
+if response:
+    #Convert JSON Records to Dataframe
+    df = pd.DataFrame()
+    for item in response['records'][0]:
+            df = df.append(pd.DataFrame.from_records(item['source']))
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    #Plot the chart
+    plot_chart(df)
     `,
 };
 
