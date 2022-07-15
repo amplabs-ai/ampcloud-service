0163b739dc3550d7fedd2eb704a5f31df23f0702
EC2 Default User <ec2-user@ip-172-31-18-6.ap-south-1.compute.internal>
2022-07-15 14:14:47 +0000

    Minor Fix

--- app/archive_constants.py
+++ app/archive_constants.py
@@ -14,7 +14,7 @@ GA_API_HOST = ""
 STRIPE_ENDPOINT_SECRET = 'whsec_NvFfYQYyWentyw5Cts5vSk6Tq410cTP3'
 STRIPE_API_KEY = "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"
 # Amplabs DB
-AMPLABS_DB_URL = "postgresql://mrs_tutorial:App4ever#@battery-archive-prod.cczwnfd9o32m.ap-south-1.rds.amazonaws.com:5432/mrs_tutorial" if ENV == "production" \
+AMPLABS_DB_URL = "postgresql://mrs_tutorial:App4ever#@amplabs-db.cczwnfd9o32m.ap-south-1.rds.amazonaws.com:5432/mrs_tutorial" if ENV == "production" \
                  else "postgresql://mrs_tutorial_dev:App4ever#@battery-archive-dev-database.cczwnfd9o32m.ap-south-1.rds.amazonaws.com:5432/mrs_tutorial"
 # linkedin share constants
 LINKEDIN_CLIENT_ID = '78opgrui0xx0ng'
