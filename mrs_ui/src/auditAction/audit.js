d4050048bc5908172c167ccc9ce371a19567fd01
Hema-Appperfect <hema@appperfect.com>
2022-07-18 18:36:55 +0530

    Added mixpanel logging on landing page.

--- mrs_ui/src/auditAction/audit.js
+++ mrs_ui/src/auditAction/audit.js
@@ -1,9 +1,7 @@
 import mixpanel from "mixpanel-browser";
 
-mixpanel.init("1808e36e2fdfd13139a1df86c970aa1b", { debug: true });
-
 export const audit = (action, user) => {
-	mixpanel.identify(user.email);
+	mixpanel.identify("user", user.email);
 	mixpanel.people.set(user);
 	mixpanel.track(action, user);
 };
