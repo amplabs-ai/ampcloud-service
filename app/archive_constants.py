51536c4efe2da6de56911f2ee7a6ec22a1b67fdb
Hema-Appperfect <hema@appperfect.com>
2022-07-29 19:53:56 +0530

    API Server: Removed hardcoded tokens/secrets/keys and database urls.

--- app/archive_constants.py
+++ app/archive_constants.py
@@ -1,5 +1,11 @@
 from enum import Enum, auto
-ENV = "production"
+from dotenv import load_dotenv, dotenv_values, find_dotenv
+from pathlib import Path
+import os
+
+load_dotenv(find_dotenv())
+
+ENV = os.getenv('ENV')
 SLASH = "/"
 
 DEGREE = 3
@@ -11,20 +17,20 @@ DB_URL = LIVE_DB_URL
 GA_API_HOST = ""
 
 # Stripe
-STRIPE_ENDPOINT_SECRET = 'whsec_NvFfYQYyWentyw5Cts5vSk6Tq410cTP3'
-STRIPE_API_KEY = "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"
+STRIPE_ENDPOINT_SECRET = os.getenv('STRIPE_PROD_ENDPOINT_SECRET') if ENV == "production" else os.getenv('STRIPE_DEV_ENDPOINT_SECRET') 
+STRIPE_API_KEY = os.getenv('STRIPE_PROD_API_KEY') if ENV == "production" else os.getenv('STRIPE_DEV_API_KEY') 
 # Amplabs DB
-AMPLABS_DB_URL = "postgresql://mrs_tutorial:App4ever#@amplabs-db.cczwnfd9o32m.ap-south-1.rds.amazonaws.com:5432/mrs_tutorial" if ENV == "production" \
-                 else "postgresql://mrs_tutorial_dev:App4ever#@dev-db.cczwnfd9o32m.ap-south-1.rds.amazonaws.com:5432/mrs_tutorial"
+AMPLABS_DB_URL = os.getenv('AMPLABS_PROD_DB_URL') if ENV == "production" \
+                 else os.getenv('AMPLABS_DEV_DB_URL')
 # linkedin share constants
-LINKEDIN_CLIENT_ID = '78opgrui0xx0ng'
-LINKEDIN_CLIENT_SECRET = 'hGAIpqZh5swxgM6v'
+LINKEDIN_CLIENT_ID = os.getenv('LINKEDIN_CLIENT_ID')
+LINKEDIN_CLIENT_SECRET = os.getenv('LINKEDIN_CLIENT_SECRET')
 LINKEDIN_REDIRECT_URI_DASH_CYCLE= 'https://www.amplabs.ai/dashboard' if ENV == 'production' else 'https://localhost:3000/dashboard/cycle-test'
 LINKEDIN_REDIRECT_URI_DASH_ABUSE= 'https://www.amplabs.ai/dashboard/abuse-test' if ENV == 'production' else 'http://localhost:3000/dashboard/abuse-test'
 
 #Auth0
-AUTH0_DOMAIN = "auth.amplabs.ai" if ENV == 'production' else "dev-lnxjzkkj.us.auth0.com"
-AUTH0_AUDIENCE = "https://amplabs.server"
+AUTH0_DOMAIN = os.getenv('AUTH0_PROD_DOMAIN') if ENV == 'production' else os.getenv('AUTH0_DEV_DOMAIN')
+AUTH0_AUDIENCE = os.getenv('AUTH0_AUDIENCE')
 AUTH0_ALGORITHMS = ["RS256"]
 
 # Public Database emails:
